9dbc09ffbbe5d6c773242618077d0a8b5a2c17b1
Miraj <mirajholmes@gmail.com>
2019-03-27 23:38:37 +0530

    Updates

---
+++ precache-manifest.01cbbcb4d0705bc7c6262cf4ca3c4cf3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8864e43a7c64a773b43",
+    "url": "/web-assist/static/js/runtime~main.2537f6a2.js"
+  },
+  {
+    "revision": "3604af6d2c742cf03e8b",
+    "url": "/web-assist/static/js/main.02543aea.chunk.js"
+  },
+  {
+    "revision": "2abd2343021ef74ad1ea",
+    "url": "/web-assist/static/js/2.3d965248.chunk.js"
+  },
+  {
+    "revision": "2abd2343021ef74ad1ea",
+    "url": "/web-assist/static/css/2.cff3fc8d.chunk.css"
+  },
+  {
+    "revision": "785aede9db054a1f329f12136821ddcd",
+    "url": "/web-assist/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/web-assist/precache-manifest.6091745358a2a1f37a15948f3ccdd364.js"
+  "/web-assist/precache-manifest.01cbbcb4d0705bc7c6262cf4ca3c4cf3.js"
 );
 
 workbox.clientsClaim();
