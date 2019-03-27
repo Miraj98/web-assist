beb32137c4c27926d80204c62b90e5a94b9a04b4
Miraj <mirajholmes@gmail.com>
2019-03-27 23:32:03 +0530

    Updates

---
+++ precache-manifest.86a63863aed53d20904d86815b32c624.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0ea04cb16e64b6f77e85",
+    "url": "/static/js/main.3aaa7846.chunk.js"
+  },
+  {
+    "revision": "f6c3c2ba2c883764fa8b",
+    "url": "/static/js/2.98b84ebb.chunk.js"
+  },
+  {
+    "revision": "f6c3c2ba2c883764fa8b",
+    "url": "/static/css/2.cff3fc8d.chunk.css"
+  },
+  {
+    "revision": "b48ebfef6ff9f385d1d0fc2579061748",
+    "url": "/index.html"
+  }
+];
